ccc56e62982de8ace6b97909c9f529e3cc921b08
Ivan Shokurov <schokuroff@icloud.com>
2021-07-23 03:35:35 +0800

    added blank xcode proj

---
+++ Snorrify/SnorrifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnorrifyApp.swift
+//  Snorrify
+//
+//  Created by Ivan Shokurov (G1) on 23.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SnorrifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
