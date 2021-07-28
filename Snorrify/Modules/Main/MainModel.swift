6f54dba182786faaedfca1f6dd59c3bc88c84f30
Ivan Shokurov <schokuroff@icloud.com>
2021-07-28 16:18:27 +0800

    f/arch: added unit tests for image extension

---
+++ Snorrify/Modules/Main/MainModel.swift
@@ -0,0 +1,9 @@
+//
+//  MainModel.swift
+//  Snorrify
+//
+//  Created by Ivan Shokurov (G1) on 28.07.2021.
+//  Copyright © 2021 Ivan Shokurov. All rights reserved.
+//
+
+import Foundation
---
+++ Snorrify/Modules/Main/MainViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MainViewModel.swift
+//  Snorrify
+//
+//  Created by Ivan Shokurov (G1) on 28.07.2021.
+//  Copyright © 2021 Ivan Shokurov. All rights reserved.
+//
+
+import Foundation
