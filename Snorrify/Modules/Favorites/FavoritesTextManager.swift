cdaada5b2849817ad0c786aa2c0f74d19bb07627
Ivan Shokurov <schokuroff@icloud.com>
2021-08-10 13:28:14 +0800

    f/fave: a new API for FavoritesTextManager

--- Snorrify/Modules/Favorites/FavoritesTextManager.swift
+++ Snorrify/Modules/Favorites/FavoritesTextManager.swift
@@ -2,6 +2,38 @@ import Foundation
 
 struct FavoritesTextManager {
     // MARK: - Aliases
+    
+    private typealias LK = LocalizationKey
+    
     // MARK: - Strings
+    
+    var favorites: String {
+        LK.favorites.localizedString
+    }
+    
+    var search: String {
+        LK.search.localizedString
+    }
+    
+    var placeholder: String {
+        LK.Favorites.placeholderDescription.localizedString
+    }
+    
+    var noResults: (title: String, description: String) {
+        let title = LK.noResults.localizedString
+        let description = LK.Search.Placeholder.tryAnotherSearch.localizedString
+        return (title, description)
+    }
+    
+    var error: String {
+        LocalizationKey.error.localizedString
+    }
+    
+    var loading: String {
+        LocalizationKey.loading.localizedString
+    }
+    
     // MARK: - Mock / Preview
+    
+    static var mock: Self = .init()
 }
