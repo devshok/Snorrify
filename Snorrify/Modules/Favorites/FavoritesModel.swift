e132877158200bfe7b96b88479cb3dbb46d4d38a
Ivan Shokurov <schokuroff@icloud.com>
2021-08-10 14:01:10 +0800

    f/fave: added API for FavoritesModel

--- Snorrify/Modules/Favorites/FavoritesModel.swift
+++ Snorrify/Modules/Favorites/FavoritesModel.swift
@@ -1,19 +1,91 @@
-import Foundation
+import Combine
 
 final class FavoritesModel: ObservableObject {
     // MARK: - Properties
+    
+    private let dbKit: DBKit
+    private var events: Set<AnyCancellable> = []
+    private var allItems: [DBFaveItemResponse] = []
+    
+    private var searchText: String = "" {
+        didSet {
+            data = filteredItems.sortedDescending()
+        }
+    }
+    
     // MARK: - Publishers
+    
+    @Published
+    var data: [DBFaveItemResponse] = []
+    
     // MARK: - Life Cycle
+    
+    init(dbKit: DBKit) {
+        self.dbKit = dbKit
+        listenEvents()
+    }
+    
+    deinit {
+        removeEvents()
+        data.removeAll()
+        debugPrint(self, #function, #line)
+    }
 }
 
 // MARK: - Events
 
-private extension FavoritesModel {}
+private extension FavoritesModel {
+    func listenEvents() {
+        dbKit.$favorites
+            .sink(receiveValue: { [weak self] items in
+                self?.allItems = items
+                self?.data = (self?.filteredItems ?? []).sortedDescending()
+            })
+            .store(in: &events)
+    }
+    
+    func removeEvents() {
+        events.forEach { $0.cancel() }
+        events.removeAll()
+    }
+}
 
 // MARK: - View Model -> Output
 
-extension FavoritesModel {}
+extension FavoritesModel {
+    func unfave(_ item: DBFaveItemResponse?) {
+        dbKit.remove(favorite: item)
+    }
+    
+    func search(for text: String) {
+        searchText = text
+    }
+}
+
+// MARK: - Internal Getters
+
+extension FavoritesModel {
+    var isSearching: Bool {
+        !searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
+}
+
+// MARK: - Private Getters
+
+private extension FavoritesModel {
+    var filteredItems: [DBFaveItemResponse] {
+        switch isSearching {
+        case true:
+            return allItems
+                .filter { $0.item?.word.contains(searchText) ?? false }
+        case false:
+            return allItems
+        }
+    }
+}
 
 // MARK: - Mock / Preview
 
-extension FavoritesModel {}
+extension FavoritesModel {
+    static var mock: FavoritesModel = .init(dbKit: .shared)
+}
